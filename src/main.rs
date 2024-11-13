00c3695596cb118bd509cbbdf7d751de06fb592b
ThatBooRadley <72572968+ThatBooRadley@users.noreply.github.com>
2024-11-13 17:26:15 -0600

    added enum

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,13 @@ fn main() {
     let mut window_controller = WindowController::new("title", WIDTH, HEIGHT, Scale::X4, true);
     window_controller.matrix.values.fill(500);
 
-    let mut matrix = Matrix::new(5, 5, false);
+    let mut matrix = Matrix::new(25, 25, false);
 
     while window_controller.window.is_open() && !window_controller.window.is_key_down(Key::Escape) {
         matrix
             .enumerate_mut()
-            .for_each(|(x, y, item)| *item = x as u32 + *item * y as u32);
-        window_controller.matrix.overlay(&matrix, 95, 95);
+            .for_each(|(x, y, item)| *item = 1 + x as u32 + *item * (y as u32 + 1));
+        window_controller.matrix.overlay(&matrix, 25, 25);
         window_controller.update()
     }
-
-    println!(
-        "og: {:?}\n clamp {:?}",
-        matrix.enumerate().collect::<Vec<_>>(),
-        matrix.clamp(2, 2, 2, 2).collect::<Vec<_>>()
-    );
 }
--- src/tools/matrix.rs
+++ src/tools/matrix.rs
@@ -24,7 +24,7 @@ impl<T: Default + Clone + Sync + Send> Matrix<T> {
         }
     }
 
-    fn bound(&self, x: usize, y: usize) -> Option<usize> {
+    fn some_bound(&self, x: usize, y: usize) -> Option<usize> {
         match (self.width, self.height, self.wrapping) {
             (w, h, true) => Some((x % w) + ((y % h) * w)),
             (w, h, false) if w > x && h > y => Some(x + (y * w)),
@@ -33,7 +33,7 @@ impl<T: Default + Clone + Sync + Send> Matrix<T> {
     }
 
     pub fn get(&self, x: usize, y: usize) -> Option<&T> {
-        if let Some(index) = self.bound(x, y) {
+        if let Some(index) = self.some_bound(x, y) {
             self.values.get(index)
         } else {
             None
@@ -41,11 +41,25 @@ impl<T: Default + Clone + Sync + Send> Matrix<T> {
     }
 
     pub fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut T> {
-        if let Some(index) = self.bound(x, y) {
+        if let Some(index) = self.some_bound(x, y) {
             self.values.get_mut(index)
         } else {
             None
         }
+    }
+
+    /// will always return value. If out of bounds, it will wrap until in bounds.
+    pub fn get_wrap(&self, x: usize, y: usize) -> &T {
+        self.values
+            .get((x % self.width) + ((y % self.height) * self.width))
+            .unwrap()
+    }
+
+    /// will always return value. If out of bounds, it will wrap until in bounds.
+    pub fn get_wrap_mut(&mut self, x: usize, y: usize) -> &mut T {
+        self.values
+            .get_mut((x % self.width) + ((y % self.height) * self.width))
+            .unwrap()
     }
 
     pub fn set(&mut self, x: usize, y: usize, value: T) {
@@ -112,54 +126,44 @@ impl<T: Default + Clone + Sync + Send> Matrix<T> {
             .flat_map(move |chunk| chunk.iter_mut().skip(x).take(width))
     }
 
-    #[cfg(feature = "parallel")]
-    pub fn par_enumerate(&self) -> impl ParallelIterator<Item = (usize, usize, &T)> {
-        self.values
-            .par_iter()
-            .enumerate()
-            .map(|(i, t)| (i % self.width, i / self.width, t))
-    }
-    #[cfg(feature = "parallel")]
-    pub fn par_enumerate_mut(&mut self) -> impl ParallelIterator<Item = (usize, usize, &mut T)> {
-        self.values
-            .par_iter_mut()
-            .enumerate()
-            .map(|(i, t)| (i % self.width, i / self.width, t))
-    }
-
-    #[cfg(feature = "parallel")]
-    pub fn par_overlay(&mut self, matrix: &Matrix<T>, x: usize, y: usize) {
-        matrix
-            .par_enumerate()
-            .for_each(|(sub_x, sub_y, value)| self.set(x + sub_x, y + sub_y, value.clone()))
-    }
-    #[cfg(feature = "parallel")]
-    pub fn par_clamp(
+    pub fn clamp_wrap(
         &self,
         x: usize,
         y: usize,
         width: usize,
         height: usize,
-    ) -> impl ParallelIterator<Item = &T> {
+    ) -> impl Iterator<Item = &T> {
         self.values
-            .par_chunks(self.width)
+            .chunks(self.width)
+            .cycle()
             .skip(y)
             .take(height)
-            .flat_map_iter(move |chunk| chunk.iter().skip(x).take(width))
+            .flat_map(move |chunk| chunk.iter().cycle().skip(x).take(width))
     }
 
-    #[cfg(feature = "parallel")]
-    pub fn par_clamp_mut(
-        &mut self,
+    /// enumerates then clamps values
+    pub fn enumerate_clamp(
+        &self,
         x: usize,
         y: usize,
         width: usize,
         height: usize,
-    ) -> impl Iterator<Item = &mut T> {
-        self.values
-            .par_chunks_mut(self.width)
-            .skip(y)
-            .take(height)
-            .flat_map_iter(move |chunk| chunk.iter_mut().skip(x).take(width))
+    ) -> impl Iterator<Item = (usize, usize, &T)> {
+        self.clamp(x, y, width, height)
+            .enumerate()
+            .map(move |(i, t)| (x + (i % self.width), y + (i / self.width), t))
+    }
+
+    /// enumerates then clamps values
+    pub fn enumerate_clamp_wrap(
+        &self,
+        x: usize,
+        y: usize,
+        width: usize,
+        height: usize,
+    ) -> impl Iterator<Item = (usize, usize, &T)> {
+        self.clamp_wrap(x, y, width, height)
+            .enumerate()
+            .map(move |(i, t)| (x + (i % self.width), y + (i / self.width), t))
     }
 }
