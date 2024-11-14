ded8a21988fe48a8b23fd87386e341a385cdc4b3
ThatBooRadley <72572968+ThatBooRadley@users.noreply.github.com>
2024-11-13 18:12:33 -0600

    test run of tilemap success

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use minifb::{Key, Scale};
-use minifb_tile_base::{tools::matrix::Matrix, window::WindowController};
+use minifb_tile_base::{
+    tile::{Tile, TileLibrary, TileMap},
+    tools::matrix::Matrix,
+    window::WindowController,
+};
 
 const WIDTH: usize = 100;
 const HEIGHT: usize = 100;
@@ -8,13 +12,23 @@ fn main() {
     let mut window_controller = WindowController::new("title", WIDTH, HEIGHT, Scale::X4, true);
     window_controller.matrix.values.fill(500);
 
-    let mut matrix = Matrix::new(25, 25, false);
-
-    while window_controller.window.is_open() && !window_controller.window.is_key_down(Key::Escape) {
+    let mut library = TileLibrary::new();
+    for i in 0..25 {
+        let mut matrix = Matrix::new(4, 4, false);
         matrix
             .enumerate_mut()
-            .for_each(|(x, y, item)| *item = 1 + x as u32 + *item * (y as u32 + 1));
-        window_controller.matrix.overlay(&matrix, 25, 25);
+            .for_each(|(x, y, u)| *u = x as u32 * y as u32 % 7 * i);
+        library.add(Tile(matrix));
+    }
+
+    let mut map = TileMap::new(25, 25, false, 4, 4);
+    map.map
+        .enumerate_mut()
+        .for_each(|(x, y, u)| *u = Some((x * y) % 25));
+    map.update_buffer(library);
+
+    while window_controller.window.is_open() && !window_controller.window.is_key_down(Key::Escape) {
+        window_controller.matrix.overlay(&map.buffer, 0, 0);
         window_controller.update()
     }
 }
--- src/tile.rs
+++ src/tile.rs
@@ -1,6 +1,6 @@
 use crate::tools::matrix::Matrix;
 
-pub struct Tile(Matrix<u32>);
+pub struct Tile(pub Matrix<u32>);
 
 /// An individual tile
 impl Tile {
@@ -10,29 +10,32 @@ impl Tile {
 }
 
 /// Where each type of tile is stored
-pub struct TileLibrary<'a>(Vec<&'a Tile>);
+pub struct TileLibrary(Vec<Tile>);
 
-impl<'a> TileLibrary<'a> {
+impl TileLibrary {
     pub fn new() -> Self {
         Self(Vec::new())
     }
 
-    pub fn add(&mut self, tile: &'a Tile) {
+    pub fn add(&mut self, tile: Tile) {
         self.0.push(tile)
     }
 
-    pub fn get(&self, index: usize) -> Option<&&Tile> {
+    pub fn get(&self, index: usize) -> Option<&Tile> {
         self.0.get(index)
     }
 }
 
 /// Where each tile is placed
 pub struct TileMap {
-    map: Matrix<usize>,
+    /// matrix storing the tiles in the map
+    pub map: Matrix<Option<usize>>,
     /// width in pixels of each tile
     tile_width: usize,
     /// height in pixels of each tile
     tile_height: usize,
+    /// pixel rendition of map
+    pub buffer: Matrix<u32>,
 }
 
 impl TileMap {
@@ -47,6 +50,18 @@ impl TileMap {
             map: Matrix::new(width, height, wrapping),
             tile_width,
             tile_height,
+            buffer: Matrix::new(width * tile_width, height * tile_height, wrapping),
         }
+    }
+
+    pub fn update_buffer(&mut self, tile_library: TileLibrary) {
+        self.map.enumerate().for_each(|(x, y, u)| {
+            if let Some(index) = u {
+                if let Some(tile) = tile_library.get(*index) {
+                    self.buffer
+                        .overlay(&tile.0, x * self.tile_width, y * self.tile_height)
+                }
+            }
+        })
     }
 }
